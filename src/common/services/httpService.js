4cd127c9896830fef6fd18a8adcbdfd8b14db7a8
Joseph Adah <jonathanadahjoe@gmail.com>
2019-07-23 12:40:27 +0100

    increase query per page fetch

--- src/common/services/httpService.js
+++ src/common/services/httpService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clientId = process.env.VUE_APP_UNSPLASH_CLIENT_ID;
-const unsplashApiUrl = `https://api.unsplash.com/search/photos/?client_id=${clientId}&page=1&per_page=8&query=`;
+const unsplashApiUrl = `https://api.unsplash.com/search/photos/?client_id=${clientId}&page=1&per_page=9&query=`;
 
 export const get = (query = 'African') => {
   return axios.get(`${unsplashApiUrl}${query}`);
